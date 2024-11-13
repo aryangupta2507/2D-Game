9bd6bdb0ef63e02bc927c91e5a8360b6b3601906
aryangupta2507 <aryangupta2504@gmail.com>
2024-11-13 21:28:27 +0530

    completed

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC. I made my first 2-D game using this which involves the use of javascript , html ,css and kaboom.
+         I am looking forward to make many more projects in the future :)
+        `,
+    "cs-degree": `This is my future CS degree, which I will get in 2027 from IIIT Kota`,
+    "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+      I also watch programming tutorials on YouTube.  
+      `,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+     
+     `,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `gonna upload soon :)`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+    Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+    Kaboom.js by watching some of my tutorials.`,
+  library: `There are lot of fictional books which I read. `,
+  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+};
